2e5a4f819610c4e090dc636bac7983950ceb5ec8
Olubisi David <oludavidconnect@gmail.com>
2020-06-10 03:09:05 +0100

    .

---
+++ backend/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9ee61073-0d5d-46e5-9d8f-fdb67f0cc251'
+};
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as MusicTab } from './Tab';
+export { default as Songs } from './Songs';
